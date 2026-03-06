1288a0162a55a79b5a4f526d0abb243d8f8cb846
Kredden <kredden@proactiveapathy.com>
2026-03-05 18:14:19 -0600

    * Fix ASK issue

--- apps/web/src/hooks/usePushNotifications.ts
+++ apps/web/src/hooks/usePushNotifications.ts
@@ -75,7 +75,7 @@ export function usePushNotifications() {
       const reg = await navigator.serviceWorker.ready;
       const sub = await reg.pushManager.subscribe({
         userVisibleOnly: true,
-        applicationServerKey: urlBase64ToUint8Array(public_key).buffer as ArrayBuffer,
+        applicationServerKey: urlBase64ToUint8Array(public_key),
       });
 
       const subJson = sub.toJSON();
